55e3046fa191b71921c4132ff5d7da151bc6e3e3
GitHub Action <action@github.com>
2024-12-01 09:00:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/521wangshunfu/zoolinascript/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/521wangshunfu/zoolinascript/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.521wangshunfu.zoolinascript.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
